5b75888a65c1160788c9210ed6f6638bbcdc8439
Jeremy Danyow <jdanyow@gmail.com>
2016-02-21 18:33:31 -0500

    zzzzzzz

--- config2.js
+++ config2.js
@@ -7,9 +7,9 @@ System.config({
     "optional": []
   },
   paths: {
-    "github:*": "https://cdn.rawgit.com/jdanyow/aurelia-plunker/v0.11.5/jspm_packages/github/*",
-    "npm:*": "https://cdn.rawgit.com/jdanyow/aurelia-plunker/v0.11.5/jspm_packages/npm/*",
-    "aurelia-bundle": "https://cdn.rawgit.com/jdanyow/aurelia-plunker/v0.11.5/dist/aurelia-bundle"
+    "github:*": "https://cdn.rawgit.com/jdanyow/aurelia-plunker/v0.11.7/jspm_packages/github/*",
+    "npm:*": "https://cdn.rawgit.com/jdanyow/aurelia-plunker/v0.11.7/jspm_packages/npm/*",
+    "aurelia-bundle": "https://cdn.rawgit.com/jdanyow/aurelia-plunker/v0.11.7/dist/aurelia-bundle"
   },
   bundles: {
     "aurelia-bundle": [
